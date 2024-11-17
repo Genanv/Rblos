50f4fca7d1d4241ccd3afd3064e1bcb4702fa80d
Genanv <77456343+Genanv@users.noreply.github.com>
2024-11-16 22:13:30 -0800

    Update EspLib.lua

--- Misc/EspLib.lua
+++ Misc/EspLib.lua
@@ -306,7 +306,7 @@ function Object:GetQuad() -- Gets a table of positions for use in pretty much ev
     local TopLeft, TopLeftOnScreen = ESP:GetScreenPosition((Pivot * CFrame.new(X, Y, 0)).Position)--[[Pivot + (Size / 2))]]
     local BottomLeft, BottomLeftOnScreen = ESP:GetScreenPosition((Pivot * CFrame.new(X, -Y, 0)).Position) --[[+ ((Size * Vector3.new(1, -1, 0)) / 2))]]
     local BottomRight, BottomRightOnScreen = ESP:GetScreenPosition((Pivot * CFrame.new(-X, -Y, 0)).Position)--[[ - (Size / 2))]]
-    local HealthBarFrom = Vector2.new(BoxPosition.X - 5, BoxPosition.Y + BoxSize.Y)
+    local HealthBarFrom = ESP:GetScreenPosition((Pivot * CFrame.new(0, -Y, 0)).Position) - Vector2.new(5, 0)
     
     if TopRightOnScreen or TopLeftOnScreen or BottomLeftOnScreen or BottomRightOnScreen then -- Boxes don't cause weird drawing issues if any part of the character is on-screen (only checks the bounding box, a player's arm can be slightly poking out and the box won't draw).
         local Positions = {
